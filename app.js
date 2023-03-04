ba92177265573d61daa86743d1097d51c56c5bde
NOMULA SRIMLIKA <96129503+Srimlika@users.noreply.github.com>
2023-03-04 11:52:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,69 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-mongoose.connect("mongodb://localhost/file_mapping_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const fileMappingSchema = new mongoose.Schema({
-  originalName: String,
-  requestedName: String,
-});
-
-const FileMapping = mongoose.model("FileMapping", fileMappingSchema);
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/:requestedName", async (req, res) => {
-  const { requestedName } = req.params;
-  const fileMapping = await FileMapping.findOne({ requestedName });
-  if (!fileMapping) {
-    return res.status(404).send("File not found");
+const fileMappings = {
+  "name": "title.jpg",
+  "another-name": "image.png",
+
+};
+
+
+app.get('/:name', (req, res) => {
+  const requestedName = req.params.name;
+
+
+  if (fileMappings.hasOwnProperty(requestedName)) {
+    const originalName = fileMappings[requestedName];
+    const extension = path.extname(originalName);
+
+    
+    const contentType = getContentType(extension);
+
+
+    const filePath = path.join(__dirname, 'public', originalName);
+    const readStream = fs.createReadStream(filePath);
+    res.set('Content-Type', contentType);
+    readStream.pipe(res);
+  } else {
+    
+    res.status(404).send('File not found');
   }
-
-  const filePath = path.join(__dirname, "public", fileMapping.originalName);
-  res.sendFile(filePath);
 });
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+// Start the server
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+
+function getContentType(extension) {
+  switch (extension) {
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    case '.png':
+      return 'image/png';
+    case '.jpg':
+      return 'image/jpeg';
+    case '.gif':
+      return 'image/gif';
+    default:
+      return 'application/octet-stream';
+  }
+}
+const mongoose = require('mongoose');
+
+
+
+const fileMappingSchema = new mongoose.Schema({
+  name: String,
+})
